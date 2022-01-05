e3dc8ce8348e18c70374a32a82925ad2f37a5e9c
SyLu4N <luaan.carlos@hotmail.com>
2022-01-05 17:56:23 -0300

    att

---
+++ aulas/aula11/index.js
@@ -0,0 +1,9 @@
+const nome = document.getElementsByName('nome');
+const sobrenome = document.getElementsByName('sobrenome');
+const peso = document.getElementsByName('peso');
+const altura = document.getElementsByName('altura');
+
+console.log(nome);
+
+document.div.innerHTML(nome);
+
