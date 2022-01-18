bde10234a8e0fba6600445ece218cb77d1dc7681
SyLu4N <luaan.carlos@hotmail.com>
2022-01-18 13:49:58 -0300

    att

--- aulas/aula27/index.js
+++ aulas/aula27/index.js
@@ -1,15 +1,17 @@
 
 function divisivel(numero) {
   if(numero % 5 === 0 && numero % 3 === 0) return 'FizzBuzz';
-  if (numero % 3 === 0) return 'Fizz';
+  if(numero % 3 === 0) return 'Fizz';
   if(numero % 5 === 0) return 'Buzz';
   return 'Numero de veado';
 }
 
 for (let i = 0; i <= 100; i++){
-  console.log(i, divisivel('a'));
-  if(typeof numero !== 'number') {
+
+  if(typeof a !== 'number') {
     console.log('NÚMERO ANIMAL!'); 
     break;
   }
+
+  console.log(i, divisivel(i));
 }
