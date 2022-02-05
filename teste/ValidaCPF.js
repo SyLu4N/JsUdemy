2ffe040c1cbfdf573d007225dda7eb65c24be471
Luan Simões <85505258+SyLu4N@users.noreply.github.com>
2022-02-05 00:29:43 -0300

    att

--- teste/ValidaCPF.js
+++ teste/ValidaCPF.js
@@ -1,19 +1,3 @@
-// 705.484.450-52 070.987.720-03
-/*
-7x  0x 5x 4x 8x 4x 4x 5x 0x
-10  9  8  7  6  5  4  3  2
-70  0  40 28 48 20 16 15 0 = 237
-
-11 - (237 % 11) = 5 (Primeiro dígito)
-Se o número digito for maior que 9, consideramos 0.
-
-7x  0x 5x 4x 8x 4x 4x 5x 0x 5x
-11 10  9  8  7  6  5  4  3  2
-77  0  45 32 56 24 20 20 0  10 = 284
-
-11 - (284 % 11) = 2 (Primeiro dígito)
-Se o número digito for maior que 9, consideramos 0.
-*/
 function ValidaCPF(cpfEnviado) {
   Object.defineProperty(this, 'cpfLimpo', {
     enumerable: true,
@@ -54,11 +38,3 @@ ValidaCPF.prototype.isSequencia = function() {
   const sequencia = this.cpfLimpo[0].repeat(this.cpfLimpo.length);
   return sequencia === this.cpfLimpo;
 };
-
-const cpf = new ValidaCPF('070.987.720-03');
-
-if(cpf.valida()) {
-  console.log('Cpf válido');
-} else {
-  console.log('Cpf inválido');
-}
