0108316b4af0c60350ad18dfb4937e84a4553afd
Luan Simões <85505258+SyLu4N@users.noreply.github.com>
2022-02-12 01:55:04 -0300

    att

--- exercicios/Cronometro/index.js
+++ exercicios/Cronometro/index.js
@@ -1,23 +1,22 @@
 function Cronometro() {
   this.start();
+  this.keypress();
   this.contador = 0;
   this.clickStart = 0;
-  this.timerGo;
   this.pause = document.querySelector('.pause');
   this.restart = document.querySelector('.restart');
   this.timer = document.querySelector('.timer');
   this.iniciar = document.querySelector('.start');
   this.btnNone = document.querySelectorAll('.btnNone')
-  this.iniciar.setAttribute('title', '"Enter" p/ começar')
   this.pause.setAttribute('title', '"P" p/ pausar');
+  this.iniciar.setAttribute('title', '"Enter" p/ começar')
   this.restart.setAttribute('title', '"R" p/ resetar');
-  this.keypress();
 }  
 
 Cronometro.prototype.start = function () {
   document.addEventListener('click', e =>{
-    const el = e.target
-    if(el.classList.contains('start')) this.startCronometro();
+    const el = e.target;
+    if(el.classList.contains('start')) this.startCronometro(); 
     if(el.classList.contains('pause')) this.pauseCronometro();
     if(el.classList.contains('restart')) this.restartCronometro();
   });
@@ -25,10 +24,12 @@ Cronometro.prototype.start = function () {
 
 Cronometro.prototype.startCronometro = function () {
   clearInterval(this.timerGo);
+  clearInterval(this.pauseAnima);
+  clearInterval(this.pauseAnima2);
   this.iniciaCorreto();
   this.clickStart++;
   this.iniciar.innerHTML = 'Iniciar';
-  this.timer.classList.remove('red');
+  this.timer.style = 'color: black';
   for(buttons of this.btnNone) buttons.classList.remove('btnNone');
 }
 
@@ -57,23 +58,35 @@ Cronometro.prototype.criaSegundos = function (segundos) {
 
 Cronometro.prototype.pauseCronometro = function () {
   clearInterval(this.timerGo);
-  this.timer.classList.add('red');
-  this.iniciar.innerHTML = 'Retornar'
+  this.timer.style = 'color: rgb(192, 0, 0)';
+  this.iniciar.innerHTML = 'Retornar';
+  this.piscaTimer();
+}
+
+Cronometro.prototype.piscaTimer = function () {
+  this.pauseAnima = setInterval(() => {
+    this.timer.style = 'color: #F8F8F8';
+  }, 500);
+
+  this.pauseAnima2 = setInterval(() => {
+    this.timer.style = 'color: rgb(192, 0, 0)';
+  }, 1000);
 }
 
 Cronometro.prototype.restartCronometro = function () {
   clearInterval(this.timerGo);
-  this.timer.innerHTML = '00:00:00'
+  clearInterval(this.pauseAnima);
+  clearInterval(this.pauseAnima2);
+  this.timer.innerHTML = '00:00:00';
   this.contador = 0;
-  this.timer.classList.remove('red')
-  this.iniciar.innerHTML = 'Iniciar'
+  this.timer.style = 'color: black';
+  this.iniciar.innerHTML = 'Iniciar';
   this.clickStart = 0;
 }
 
 Cronometro.prototype.keypress = function () {
   document.addEventListener('keypress', e =>{
     const el = e.keyCode;
-    console.log(el);
     if(el === 112) this.pauseCronometro();
     if(el === 13) this.startCronometro();
     if(el === 114) this.restartCronometro();
